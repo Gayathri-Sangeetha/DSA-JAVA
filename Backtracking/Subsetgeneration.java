e8ca608e6e778d433f3765128d9c569494667d4c
Gayathri-Sangeetha <gayathrisangeetha28@gmail.com>
2026-01-21 11:12:59 +0530

    Subset target sum

---
+++ Backtracking/Subsetgeneration.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Subsetgeneration{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        //subset sum
+        int targetsum=89;
+        int[]arr={1,3,7,5};
+        if(subset(arr,0,0,targetsum)){
+            System.out.println("yes");
+            return;
+        }
+        System.out.println("No");}
+        public static boolean subset(int[] arr,int index,int sum,int targetsum){
+            if(sum==targetsum){
+                return true;
+            }
+            if(index>=arr.length){
+                return false;
+            }
+            if(subset(arr,index+1,sum+arr[index],targetsum)){
+                return true;
+            }
+       return subset(arr,index+1,sum,targetsum);
+        }}
+
